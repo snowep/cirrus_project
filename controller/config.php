ec896e27b9e037de903cf7c66609ba30d63d6097
/snowep <42274762+snowep@users.noreply.github.com>
2022-08-12 10:21:50 +0800

    init

--- controller/config.php
+++ controller/config.php
@@ -10,4 +10,9 @@
     if($db->connect_errno > 0){
         die('Unable to connect to database [' . $db->connect_error . ']');
     }
+
+    $dbdemo = new mysqli(DB_HOST, DB_USER, DB_PASS, 'cirrus_v2');
+    if($dbdemo->connect_errno > 0){
+        die('Unable to connect to database [' . $db->connect_error . ']');
+    }
 ?>
